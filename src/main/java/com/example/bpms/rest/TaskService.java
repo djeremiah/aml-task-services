0a5f36392baede84b157db8f1446962417a6b7bb
David Murphy <murphy.davidj@gmail.com>
2015-07-27 11:55:14 -0400

    Update display logic for average time to completion

--- src/main/java/com/example/bpms/rest/TaskService.java
+++ src/main/java/com/example/bpms/rest/TaskService.java
@@ -1,15 +1,15 @@
 package com.example.bpms.rest;
 
+import java.lang.reflect.Field;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Instant;
+import java.time.Duration;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.ZoneId;
-import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 import javax.persistence.EntityManager;
@@ -29,7 +29,7 @@ import org.kie.remote.client.api.exception.InsufficientInfoToBuildException;
 
 @Path("/task-service")
 public class TaskService {
-	
+
 	@PersistenceContext
 	EntityManager em;
 
@@ -40,8 +40,7 @@ public class TaskService {
 		factory = RemoteRuntimeEngineFactory.newRestBuilder()
 				.addUrl(new URL("http://localhost:8080/business-central"))
 				.addDeploymentId("org.kie.example:project1:1.0.1")
-				.addUserName("bpmsAdmin")
-				.addPassword("jbossadmin1!")
+				.addUserName("bpmsAdmin").addPassword("jbossadmin1!")
 				.buildFactory();
 	}
 
@@ -49,21 +48,95 @@ public class TaskService {
 	@Path("/{uid}/summary")
 	@Produces("application/json")
 	public DashboardSummary getSummary(@PathParam("uid") final String uid) {
-		TypedQuery<BAMTaskSummaryImpl> query = em.createQuery("FROM BAMTaskSummaryImpl", BAMTaskSummaryImpl.class);
+		TypedQuery<BAMTaskSummaryImpl> query = em.createQuery(
+				"FROM BAMTaskSummaryImpl", BAMTaskSummaryImpl.class);
 		List<BAMTaskSummaryImpl> bamTasks = query.getResultList();
-		
-		Predicate<BAMTaskSummaryImpl> myTasks = (t -> Objects.equals(uid, t.getUserId()));
-		Predicate<BAMTaskSummaryImpl> totalCompleted = (t -> Status.Completed.toString().equals(t.getStatus()));
-		
-		return new DashboardSummary(
-				bamTasks.size(), 
-				bamTasks.stream().filter(myTasks).count(), 
-				factory.newRuntimeEngine().getTaskService().getTasksAssignedAsPotentialOwner(uid, null).stream().map(ts ->  ts.getExpirationTime()).filter(d -> d == null || LocalDate.now().plusDays(2).isAfter(d.toInstant().atZone(ZoneId.systemDefault()).toLocalDate())).count(), 
-				bamTasks.stream().filter(totalCompleted).count(), 
-				"NEVER");
+		List<TaskSummary> runtimeTasks = factory.newRuntimeEngine()
+				.getTaskService().getTasksAssignedAsPotentialOwner(uid, null);
+
+		return new DashboardSummary(getTotalTasks(bamTasks), getMyTasks(
+				bamTasks, uid), getUrgentTasks(runtimeTasks),
+				getTotalCompleted(bamTasks),
+				getAverageTimeToCompletion(bamTasks));
 	}
-	
-	
+
+	private long getTotalTasks(List<BAMTaskSummaryImpl> bamTasks) {
+		return bamTasks.size();
+	}
+
+	private long getMyTasks(List<BAMTaskSummaryImpl> bamTasks, String uid) {
+		Predicate<BAMTaskSummaryImpl> myTasks = (t -> Objects.equals(uid,
+				t.getUserId()));
+		return bamTasks.stream().filter(myTasks).count();
+	}
+
+	private long getUrgentTasks(List<TaskSummary> runtimeTasks) {
+		Function<TaskSummary, Date> toExpirationTime = (ts -> ts
+				.getExpirationTime());
+		Predicate<Date> dueTwoDaysOrNoDueDate = d -> d == null
+				|| LocalDate
+						.now()
+						.plusDays(2)
+						.isAfter(
+								d.toInstant().atZone(ZoneId.systemDefault())
+										.toLocalDate());
+
+		return runtimeTasks.stream().map(toExpirationTime)
+				.filter(dueTwoDaysOrNoDueDate).count();
+	}
+
+	private long getTotalCompleted(List<BAMTaskSummaryImpl> bamTasks) {
+		Predicate<BAMTaskSummaryImpl> totalCompleted = (t -> Status.Completed
+				.toString().equals(t.getStatus()));
+		return bamTasks.stream().filter(totalCompleted).count();
+	}
+
+	private String getAverageTimeToCompletion(List<BAMTaskSummaryImpl> bamTasks) {
+		Function<BAMTaskSummaryImpl, Duration> timeToCompletion = (task -> {
+			try {
+				Field createdDate = BAMTaskSummaryImpl.class.getDeclaredField("createdDate");
+				createdDate.setAccessible(true);
+				Date start = (Date) createdDate.get(task);
+				Date end = task.getEndDate();
+				return Duration.between(start.toInstant(), end.toInstant());
+			} catch (IllegalArgumentException | IllegalAccessException
+					| NoSuchFieldException | SecurityException e) {
+				throw new IllegalStateException("could not retrieve createdDate for task summary. cannot calculate average",e);
+			}
+		});
+
+		Duration averageTimeToCompletion = bamTasks
+				.stream()
+				.filter(t -> t.getEndDate() != null)
+				.map(timeToCompletion)
+				.collect(() -> new DurationAverage(), DurationAverage::add,
+						DurationAverage::combine).compute();
+
+		long hours = averageTimeToCompletion.toHours();
+		long minutes = averageTimeToCompletion.minusHours(hours).toMinutes();
+		
+		return hours + "h " + minutes + "m";
+
+	}
+
+	public class DurationAverage {
+		public Duration runningSum = Duration.ZERO;
+		public long count = 0;
+
+		void add(Duration d) {
+			runningSum = runningSum.plus(d);
+			count++;
+		}
+
+		void combine(DurationAverage da) {
+			runningSum = runningSum.plus(da.runningSum);
+			count += da.count;
+		}
+
+		Duration compute() {
+			return runningSum.dividedBy(count);
+		}
+	}
 
 	@GET
 	@Path("/{uid}/tasklist")
@@ -72,7 +145,6 @@ public class TaskService {
 		return factory.newRuntimeEngine().getTaskService()
 				.getTasksAssignedAsPotentialOwner(uid, null);
 	}
-	
 
 	public class DashboardSummary {
 		private long totalTasks; // total tasks for user and group
